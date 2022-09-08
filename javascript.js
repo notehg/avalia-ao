c01c18f08d693f8ecb9e0ed79ce380d72b3ad776
notehg <110551666+notehg@users.noreply.github.com>
2022-09-08 09:34:17 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,11 @@
+
+function calcMultiplicação()
+{
+let Part = document.getElementsByName("Part")[0].value;
+let num4 = document.getElementsByName("num4")[0].value;
+    
+let sum= Number(Part) * Number(num4)
+    
+document.getElementsByName("sum")[0].value = sum;
+
+}
